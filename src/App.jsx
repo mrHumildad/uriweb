0b1f4022602cad283e2f906f6314734b34625548
mrHumildad <nghizzah@gmail.com>
2025-10-25 13:23:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const App =() =>{
+
+  return (
+    <div className='app'>
+      <div className="header">
+        <img src="src/assets/logo_header.png" alt="logo" className='logo-header'/>
+        <span className="title">ESPECTACLES I ACTIVITATS PER TOTHOM</span>
+        <div className="nav-button">X</div>
+      </div>
+      <div className="container">
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
